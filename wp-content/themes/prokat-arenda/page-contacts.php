9a5e0c1aad3e7736ba29d32c4e43d52aad57fe4a
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:47:36 +0300

    Add contacts page template

---
+++ wp-content/themes/prokat-arenda/page-contacts.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Контакты
+*/
+
+get_header();
+?>
+
+<main class="container">
+<h1>Контакты</h1>
+
+<div class="contact-info">
+<p>📞 Телефон:<br>+7 (000) 000-00-00</p>
+<p>✉ Email:<br>info@prokat-arenda.online</p>
+<p>🕒 Работаем ежедневно: 8:00–20:00</p>
+</div>
+
+<?php get_template_part('template-parts/booking-form'); ?>
+</main>
+
+<?php get_footer(); ?>
