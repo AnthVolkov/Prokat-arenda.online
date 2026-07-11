4ec8ea38bb928e9de355739c7714fcc947910506
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:22:55 +0300

    feat(theme): add initial index template

---
+++ wp-content/themes/prokat-arenda/index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<main class="container">
+<h1>Prokat Arenda</h1>
+<?php if(have_posts()): while(have_posts()): the_post(); the_content(); endwhile; endif; ?>
+</main>
+<?php get_footer(); ?>
