3e8ec7b87290491916f5aad25ff57de1ba9f5a07
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:51:24 +0300

    Add booking form fields and nonce protection

--- wp-content/themes/prokat-arenda/template-parts/booking-form.php
+++ wp-content/themes/prokat-arenda/template-parts/booking-form.php
@@ -2,10 +2,40 @@
 <h3>Заявка на аренду</h3>
 
 <form method="post">
-<p><label>Имя</label><input type="text" name="client_name" required></p>
-<p><label>Телефон</label><input type="tel" name="client_phone" required></p>
-<p><label>Оборудование</label><input type="text" name="equipment_name" value="<?php echo esc_attr(get_the_title()); ?>"></p>
+
+<?php wp_nonce_field('prokat_booking_action','prokat_booking_nonce'); ?>
+
+<p>
+<label>Имя</label>
+<input type="text" name="client_name" required>
+</p>
+
+<p>
+<label>Телефон</label>
+<input type="tel" name="client_phone" required>
+</p>
+
+<p>
+<label>Оборудование</label>
+<input type="text" name="equipment_name" value="<?php echo esc_attr(get_the_title()); ?>">
+</p>
+
+<p>
+<label>Дата аренды</label>
+<input type="date" name="rent_date">
+</p>
+
+<p>
+<label>Срок аренды</label>
+<input type="text" name="rent_period" placeholder="Например: 3 дня">
+</p>
+
+<p>
+<label>Комментарий</label>
+<textarea name="client_comment"></textarea>
+</p>
 
 <button class="button" type="submit">Отправить заявку</button>
+
 </form>
 </div>
