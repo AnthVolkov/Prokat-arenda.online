f114330fb4bd62fa391a4857b45becd3b70e8047
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:44:08 +0300

    Update Prokat Arenda index template

--- wp-content/themes/prokat-arenda/index.php
+++ wp-content/themes/prokat-arenda/index.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
+
 <main class="container">
-<h1>Prokat Arenda</h1>
+
+<h1>Prokat-arenda.online</h1>
+
+<p>Аренда инструмента и оборудования.</p>
+
 <?php if(have_posts()): while(have_posts()): the_post(); the_content(); endwhile; endif; ?>
+
 </main>
+
 <?php get_footer(); ?>
