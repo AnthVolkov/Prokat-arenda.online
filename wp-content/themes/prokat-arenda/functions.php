99270f4a7b340b71bb4862f92e032d5573eef02f
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:43:20 +0300

    Update Prokat Arenda theme functions

--- wp-content/themes/prokat-arenda/functions.php
+++ wp-content/themes/prokat-arenda/functions.php
@@ -1,15 +1,33 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-function prokat_arenda_setup(){
- add_theme_support('title-tag');
- add_theme_support('post-thumbnails');
- add_theme_support('custom-logo');
- register_nav_menus(['primary'=>'Primary Menu']);
-}
-add_action('after_setup_theme','prokat_arenda_setup');
+function prokat_setup(){
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
 
-function prokat_arenda_assets(){
- wp_enqueue_style('prokat-style', get_stylesheet_uri(), [], '0.1.0');
+    register_nav_menus([
+        'main_menu' => 'Главное меню'
+    ]);
 }
-add_action('wp_enqueue_scripts','prokat_arenda_assets');
+add_action('after_setup_theme','prokat_setup');
+
+function prokat_styles(){
+    wp_enqueue_style('prokat-style', get_stylesheet_uri(), [], '1.0');
+
+    wp_enqueue_style(
+        'prokat-main',
+        get_template_directory_uri().'/assets/css/main.css',
+        [],
+        '1.0'
+    );
+}
+add_action('wp_enqueue_scripts','prokat_styles');
+
+if (file_exists(get_template_directory().'/includes/equipment.php')) {
+    require_once get_template_directory().'/includes/equipment.php';
+}
+
+if (file_exists(get_template_directory().'/includes/booking.php')) {
+    require_once get_template_directory().'/includes/booking.php';
+}
