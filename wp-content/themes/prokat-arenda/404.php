378cf7661f1bcd36df844552ee1349b4d196f86e
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:48:12 +0300

    Add 404 page template

---
+++ wp-content/themes/prokat-arenda/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main class="container">
+<h1>Страница не найдена</h1>
+<p>Вернитесь на главную страницу или выберите оборудование.</p>
+<a class="button" href="<?php echo home_url(); ?>">На главную</a>
+</main>
+
+<?php get_footer(); ?>
