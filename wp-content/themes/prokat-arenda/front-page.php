9aba6fd5c8cad5d34fd4189675632fd12aea4f53
AnthVolkov <hibine01@gmail.com>
2026-07-11 15:24:41 +0300

    feat(theme): add front page template

---
+++ wp-content/themes/prokat-arenda/front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<main>
+<section class="hero">
+<div class="container">
+<h1>Аренда строительного инструмента и оборудования</h1>
+<p>Надежная техника, честные цены и быстрая выдача.</p>
+<a href="#catalog">Перейти в каталог</a>
+</div>
+</section>
+<section id="catalog"><div class="container"><h2>Популярные категории</h2><ul><li>Перфораторы</li><li>Бетономешалки</li><li>Генераторы</li><li>Лестницы</li></ul></div></section>
+</main>
+<?php get_footer(); ?>
